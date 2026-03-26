8902faffdb1b40d5170d998195c5c305404be35b
ByteVoid <es.yuviz19@gmail.com>
2026-03-26 22:17:51 +0530

    add basic feature of links availibility

--- firefox_startup/scripts/script.js
+++ firefox_startup/scripts/script.js
@@ -1,5 +1,31 @@
 const form = document.getElementById("searchform");
 const input = document.getElementById("searchinput");
+const linksul = document.getElementById("links");
+const linkbutt = document.getElementById("newLinkbutt");
+
+let links = [
+  { name: "Youtube", url: "https://www.youtube.com/" },
+  { name: "YTMusic", url: "https://music.youtube.com/" }
+];
+
+linkbutt.addEventListener("click", function (e) {
+
+});
+
+function renderList() {
+  linksul.innerHTML = "";
+  for (const link of links) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = link.url;
+    a.textContent = link.name;
+    a.target = "_blank";
+    li.appendChild(a);
+    linksul.appendChild(li);
+  };
+};
+
+renderList();
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
